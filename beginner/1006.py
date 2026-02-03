7c28a208103f732d056e60a1306316e2abe198ea
witcliff-byte <simukavipnew@protonmail.com>
2026-02-03 00:57:22 +0000

    solved 1006.py

---
+++ beginner/1006.py
@@ -0,0 +1,9 @@
+# 1006 - Average 2
+# https://judge.beecrowd.com/en/problems/view/1006
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+avg = (a*2+b*3+c*5) / (2+3+5)
+print(f"MEDIA = {avg:.1f}")
