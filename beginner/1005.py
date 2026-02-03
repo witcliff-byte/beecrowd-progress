fcc8fc63a42062a6d1bb6c47fbfc38febe3b37cf
witcliff-byte <simukavipnew@protonmail.com>
2026-02-03 00:48:58 +0000

    solved 1005.py

---
+++ beginner/1005.py
@@ -0,0 +1,9 @@
+# 1005 - Average 1
+# https://judge.beecrowd.com/en/problems/view/1005
+# 03/02/2026
+
+a= float(input())
+b= float(input())
+
+avg = (a*3.5+b*7.5) / (3.5+7.5)
+print(f"MEDIA = {avg:.5f}")
