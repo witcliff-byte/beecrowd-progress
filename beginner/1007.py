5ae853059f24f04b1de6e4f19a6358bdbabf3e75
witcliff-byte <simukavipnew@protonmail.com>
2026-02-03 01:00:18 +0000

    solved 1007.py

---
+++ beginner/1007.py
@@ -0,0 +1,10 @@
+# 1007 - Difference
+# https://judge.beecrowd.com/en/problems/view/1007
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+diff = (a*b-c*d)
+print(f"DIFERENCA = {diff}")
