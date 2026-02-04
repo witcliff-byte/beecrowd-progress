22f2c402030c505b321334f6ecbc038e78b77d04
witcliff-byte <simukavipnew@protonmail.com>
2026-02-04 18:00:27 +0000

    solved 1017.py

---
+++ beginner/1017.py
@@ -0,0 +1,8 @@
+# 1017 - Fuel Spent
+# https://www.beecrowd.com.br/judge/en/problems/view/1017
+
+spent_time = int(input())
+average_spead = int(input())
+
+distance = (spent_time * average_spead) / 12
+print(f"{distance:.3f}")
