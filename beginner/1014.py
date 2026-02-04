2f58c33a2b63a1c9bbc0193abcb79782d6bd16cf
witcliff-byte <simukavipnew@protonmail.com>
2026-02-04 15:02:49 +0000

    solved 1014.py

---
+++ beginner/1014.py
@@ -0,0 +1,9 @@
+# 1014 - Consumption
+# https://www.beecrowd.com.br/judge/en/problems/view/1014
+
+
+distance = int(input())
+spent_fuel_total = float(input())
+
+consumption = distance / spent_fuel_total
+print(f"{consumption:.3f} km/l")
